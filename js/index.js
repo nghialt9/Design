8987c3f3867b1442d1fe2c07471151a2665810ae
DESKTOP-78FU1CJ\NghiaLT <lamtrongnghia1990@gmail.com>
2019-06-23 09:49:05 +0700

    test

--- js/index.js
+++ js/index.js
@@ -1,71 +1,87 @@
 /* start angularjs */
 var slideshow = angular.module('app', []);
 slideshow.controller('appCtrl', function($scope, $interval) {
-    //khởi tạo hàm chạy và khai báo biến
-    var init = function() {
-        $scope.slidesShow = [{
-                numbertext: "1 / 10",
-                image: "./assets/anhbia1.jpg",
-                text: "Hình ảnh Bào Cát"
-            },
-            {
-                numbertext: "2 / 10",
-                image: "./assets/anhbia2.jpg",
-                text: "Trang trí cho Trung Thu"
-            },
-            {
-                numbertext: "3 / 10",
-                image: "./assets/anhbia3.jpg",
-                text: "Địa điểm mỗi nhà"
-            },
-            {
-                numbertext: "4 / 10",
-                image: "./assets/anhbia4.jpg",
-                text: "Thi múa hát"
-            },
-            {
-                numbertext: "5 / 10",
-                image: "./assets/anhbia5.jpg",
-                text: "Chuẩn bị phát quà"
-            },
-            {
-                numbertext: "6 / 10",
-                image: "./assets/anhbia6.jpg",
-                text: "Hại chị xinh xinh đang phát quà"
-            },
-            {
-                numbertext: "7/10",
-                image: "./assets/anhbia7.jpg",
-                text: "Anh tổ chức đang đọc bài phát biểu trước khi bắt đầu"
-            },
-            {
-                numbertext: "8 / 10",
-                image: "./assets/anhbia8.jpg",
-                text: "Anh ấy đang xitj thuốc rầy cho đậu phọng"
-            },
-            {
-                numbertext: "9 / 10",
-                image: "./assets/anhbia9.jpg",
-                text: "Poster quảng bá về hội đình thần"
-            },
-            {
-                numbertext: "10 / 10",
-                image: "./assets/anhbia10.jpg",
-                text: "Đám bắp đang tốt ở Bào Cát"
-            },
-        ]
-    }
+        //khởi tạo hàm chạy và khai báo biến
+        var init = function() {
+            $scope.slidesShow = [{
+                    numbertext: "1 / 10",
+                    image: "./assets/anhbia1.jpg",
+                    text: "Hình ảnh Bào Cát"
+                },
+                {
+                    numbertext: "2 / 10",
+                    image: "./assets/anhbia2.jpg",
+                    text: "Trang trí cho Trung Thu"
+                },
+                {
+                    numbertext: "3 / 10",
+                    image: "./assets/anhbia3.jpg",
+                    text: "Địa điểm mỗi nhà"
+                },
+                {
+                    numbertext: "4 / 10",
+                    image: "./assets/anhbia4.jpg",
+                    text: "Thi múa hát"
+                },
+                {
+                    numbertext: "5 / 10",
+                    image: "./assets/anhbia5.jpg",
+                    text: "Chuẩn bị phát quà"
+                },
+                {
+                    numbertext: "6 / 10",
+                    image: "./assets/anhbia6.jpg",
+                    text: "Hại chị xinh xinh đang phát quà"
+                },
+                {
+                    numbertext: "7/10",
+                    image: "./assets/anhbia7.jpg",
+                    text: "Anh tổ chức đang đọc bài phát biểu trước khi bắt đầu"
+                },
+                {
+                    numbertext: "8 / 10",
+                    image: "./assets/anhbia8.jpg",
+                    text: "Anh ấy đang xitj thuốc rầy cho đậu phọng"
+                },
+                {
+                    numbertext: "9 / 10",
+                    image: "./assets/anhbia9.jpg",
+                    text: "Poster quảng bá về hội đình thần"
+                },
+                {
+                    numbertext: "10 / 10",
+                    image: "./assets/anhbia10.jpg",
+                    text: "Đám bắp đang tốt ở Bào Cát"
+                },
+            ]
+        }
 
-    //chạy hàm khởi tạo
-    init();
+        //chạy hàm khởi tạo
+        init();
 
-    //hàm chạy thời gian.
-    var updateClock = function() {
-        $scope.dateNow = new Date();
-    };
-    $interval(updateClock, 1);
-    updateClock();
-});
+        //hàm chạy thời gian.
+        var updateClock = function() {
+            $scope.dateNow = new Date();
+        };
+        $interval(updateClock, 1);
+        updateClock();
+    })
+    .component('flappy', {
+        transclude: true,
+        require: {
+            tabsCtrl: '^myTabs'
+        },
+        bindings: {
+            title: '@'
+        },
+        controller: function() {
+            this.$onInit = function() {
+                this.tabsCtrl.addPane(this);
+                console.log(this);
+            };
+        },
+        templateUrl: './html/flappy.html'
+    });
 /* end angularjs */
 
 /* start sliders */
